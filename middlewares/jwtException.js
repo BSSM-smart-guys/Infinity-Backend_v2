af9b18ae59dc75d19bdec7cbe4cf9f0a3ba97621
마현우 <mhw35689402@gmail.com>
2023-07-05 14:07:22 +0900

    feat : 토큰 검증 필터

--- middlewares/jwtException.js
+++ middlewares/jwtException.js
@@ -1,14 +1,22 @@
 const jwt = require("jsonwebtoken");
 
-const jwtException = (req, res, next) => {
+const jwtException = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const verifyResult = jwt.verify(token, process.env.JWT_SECRET);
-    return res.status(200).json("success");
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        console.log("Invalid token");
+        return res.status(401).json({ error: "Invalid token" });
+      }
+    });
+    const redisInfo = {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    };
+    next();
   } catch (error) {
     return res.status(401).json({ error: "Invalid or Not exsit token" });
   }
-  next();
 };
 
 module.exports = jwtException;

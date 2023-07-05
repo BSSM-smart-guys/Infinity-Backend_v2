6cd5e3c163ebf0537fcc82eeb6c801db13c7f1cf
마현우 <mhw35689402@gmail.com>
2023-07-05 10:34:35 +0900

    feat : mysql connection 생성

---
+++ models/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2/promise");
+
+let connection;
+
+const connect = async () => {
+  if (!connection) {
+    connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+  }
+  return connection;
+};
+
+const getConnection = () => {
+  return connection;
+};
+
+module.exports = { connect, getConnection };

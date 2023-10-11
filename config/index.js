f091043c34aba4a0e0e245f40fc49c7035c85cbb
마현우 <mhw35689402@gmail.com>
2023-10-11 11:20:05 +0900

    fix: env파일 키 이름 변경

--- config/index.js
+++ config/index.js
@@ -34,7 +34,7 @@ const kakao = {
 const laas = {
   projectCode: env.LAAS_PROJECT_CODE,
   apiKey: env.LAAS_PROJECT_API_KEY,
-  hash: env.LAAS_PRESET_HASH_SAMPLE,
+  hash: env.LAAS_PRESET_HASH,
   endpoint: env.WANTED_ENDPOINT
 }
 

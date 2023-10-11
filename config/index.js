85827c2caf87b07f74eb97c3bea8d1c93f7316e0
마현우 <mhw35689402@gmail.com>
2023-10-11 11:34:27 +0900

    fix: env파일 키 이름 잘못되어 있던 점 수정

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ const laas = {
   projectCode: env.LAAS_PROJECT_CODE,
   apiKey: env.LAAS_PROJECT_API_KEY,
   hash: env.LAAS_PRESET_HASH,
-  endpoint: env.WANTED_ENDPOINT
+  endpoint: env.LAAS_ENDPOINT
 }
 
 module.exports = { development, production, test, kakao, laas };

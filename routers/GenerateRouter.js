6874623445fc8c04ab27988e8a4341da75949f47
HUN1i <nicebrian0328@gmail.com>
2023-10-14 22:44:57 +0900

    FIX :: 줄맞춤

--- routers/GenerateRouter.js
+++ routers/GenerateRouter.js
@@ -6,7 +6,7 @@ const { generateImage } = require("../service/karlo");
 
 router.post("/novel", async (req, res) => {
   const result = await callLaas(req.body);
-  res.status(200).json({ result: result });
+  res.status(200).json({ result });
 });
 
 router.post("/image", async (req, res) => {
@@ -28,7 +28,7 @@ router.post("/image", async (req, res) => {
 
   req.session.save();
 
-  res.status(200).json({ result: result });
+  res.status(200).json({ result });
 });
 
 module.exports = router;
--- service/laas.js
+++ service/laas.js
@@ -1,30 +1,33 @@
-const { laas} = require("../config");
+const { laas } = require("../config");
 const axios = require("axios");
 
 const callLaas = (dto) => {
-    let params = {};
-    Object.entries(dto).forEach(([key, value]) => params[key] = value.toString());
+  let params = {};
+  Object.entries(dto).forEach(
+    ([key, value]) => (params[key] = value.toString())
+  );
 
-    const headers = {
-        headers: {
-            project: laas.projectCode,
-            apiKey: laas.apiKey,
-            "Content-Type": "application/json"
-        }
-    }
+  const headers = {
+    headers: {
+      project: laas.projectCode,
+      apiKey: laas.apiKey,
+      "Content-Type": "application/json",
+    },
+  };
 
-    const body = {
-        hash: laas.hash,
-        params
-    }
+  const body = {
+    hash: laas.hash,
+    params,
+  };
 
-    return axios.post(laas.endpoint, body, headers)
-        .then(res => {
-            return res.data.choices[0].message.content;
-        })
-        .catch(err => {
-            return err;
-        });
-}
+  return axios
+    .post(laas.endpoint, body, headers)
+    .then((res) => {
+      return res.data.choices[0].message.content;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
 
-module.exports = { callLaas }
+module.exports = { callLaas };
